bfc380f59928b943769eda454f2c292d397e4506
Matthew Gardner <matt.gardner32@gmail.com>
2019-11-23 20:57:26 -0700

    I updated the validations. Also added nav buttons for clients and instruments. Also updated the titles on the different views.

--- BlowOut/Models/Client.cs
+++ BlowOut/Models/Client.cs
@@ -16,13 +16,22 @@ namespace BlowOut.Models
         public string FirstName { get; set; }
         [Required]
         public string LastName { get; set; }
+        [Required]
         public string Address { get; set; }
+        [Required]
         public string City { get; set; }
+        [Required]
         public string State { get; set; }
+        [Required]
+        [RegularExpression(@"\d{5}$", ErrorMessage = "Invalid Zip Code")]
         public int Zipcode { get; set; }
         [Required]
+        [RegularExpression(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$", ErrorMessage = "Please use a valid email.")]
         public string EmailAddress { get; set; }
         [Required]
+        [RegularExpression(@"\(?\d{3}\)?-? *\d{3}-? *-?\d{4}", ErrorMessage = "Please use valid # in (XXX) XXX-XXXX format")]
         public string PhoneNumber { get; set; }
+        //[RegularExpression(@"^\(?([0-9]{3})\)?[-.●]?([0-9]{3})[-.●]?([0-9]{4})$", ErrorMessage = "Please use a 10-digit number.")]
+        //public long PhoneNumber { get; set; }
     }
 }
