d1607ebbbfbc54c88253b332b08dfd4b15945e3c
gardnermt <56484421+gardnermt@users.noreply.github.com>
2019-11-23 23:33:57 -0700

    Add files via upload

---
+++ Models/Instrument.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace BlowOut.Models
+{
+    [Table("Instrument")]
+    public class Instrument
+    {
+        [Key]
+        public int InstrumentID { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public int Price { get; set; }
+        public bool New { get; set; }
+        public int ClientID { get; set; }
+    }
+}
